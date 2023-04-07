3877287df2077e6d0f299f6494cf89e4db66e878
valentin.buchon <valentin@buchon.fr>
2023-04-07 12:10:51 +0200

    Creation du dataframe et de la matrice d'adjacence en temps + zone ID

--- timeRoadCluster.py
+++ timeRoadCluster.py
@@ -6,13 +6,20 @@ road = "RouteID_00143bdd-0a6b-49ec-bb35-36593d303e77"
 with open('firstRoad.json', 'r') as f:
     data = json.load(f)
 
+with open('projet recherche/model_build_inputs/route_data.json', 'r') as f:
+    zoneRoad = json.load(f)
+
 
 name = list(list(data[road].values())[0])
+
+zoneList = [zone['zone_id'] for zone in zoneRoad[road]['stops'].values()]
+
 df = pd.DataFrame(columns=name, index=name)
 print(len(name))
 for el in name:
     row = data[road][el]
     df.loc[el] = pd.Series(row)
+df["zone_id"] = zoneList
 
 print(df)
 
