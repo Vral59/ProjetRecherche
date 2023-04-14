7e2ef38196477ca055c7f54586eb8b9181cba813
valentin.buchon <valentin@buchon.fr>
2023-04-14 12:33:49 +0200

    Fix ERP

--- eval.py
+++ eval.py
@@ -18,6 +18,7 @@ def SD(A,B):
 
     return (2/(n*(n-1)))*sd
 
+# Cette fonction revoie toujours 0 pour ERPe a corriger
 """
 Fonction récursive pour calculer ERP
 :param
@@ -28,15 +29,43 @@ cpt : compteur d'opération de substitution ect...
 :return
 liste sous la forme : [ERBnorm,ERBe]
 """
-def ERP(A,B,cpt):
-    # je ne suis pas sûr, j'ai fait une implémentation de la formule sur la page wikipedia
+def ERP(A, B, cpt):
     if len(A) == 0:
-        return [len(B),cpt + len(B)]
+        return len(B), cpt + len(B)
     if len(B) == 0:
-        return [len(A), cpt + len(A)]
+        return len(A), cpt + len(A)
     if A[0] == B[0]:
         return ERP(A[1:], B[1:], cpt)
-    return min(ERP(A[1:], B, cpt+1)[0] + 1, ERP(A, B[1:], cpt+1)[0] + 1, ERP(A[1:], B[1:], cpt+1)[0] + 1)
+    return min(ERP(A[1:], B, cpt+1)[0] + 1, ERP(A, B[1:], cpt+1)[0] + 1, ERP(A[1:], B[1:], cpt+1)[0] + 1), cpt
+
+# Vérifier les valeurs
+"""
+Fonction récursive pour calculer ERP
+:param
+A : séquence réalisée
+B : séquence soumise
+
+:return
+tuple sous la forme : [ERBnorm,ERBe]
+"""
+def LevenshteinDistance(A, B):
+    m, n = len(A), len(B)
+    D = [[0] * (n + 1) for _ in range(m + 1)]
+
+    # initialisation de la première colonne et première ligne
+    for i in range(m + 1):
+        D[i][0] = i
+    for j in range(n + 1):
+        D[0][j] = j
+
+    # calcul de la matrice de distance
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            cost = 0 if A[i - 1] == B[j - 1] else 1
+            D[i][j] = min(D[i - 1][j] + 1, D[i][j - 1] + 1, D[i - 1][j - 1] + cost)
+
+    # renvoi de la distance d'édition et du nombre d'opérations d'édition
+    return D[m][n], D[m][n] + m + n - 2 * D[m][n]
 
 """
 calcule le score de la route proposé 
@@ -49,5 +78,15 @@ SD * ERPnorm / ERPe
 """
 def routeScore(A, B):
     sd = SD(A, B)
-    erp = ERP(A, B, 0)
-    return (sd*erp[0])/erp[1]
+    #erp = ERP(A, B, 0)
+    norm,e = LevenshteinDistance(A,B)
+    print("sd = ", sd)
+    print("ERPnorm = ", norm)
+    print("ERPe = ", e)
+    return (sd*norm)/e
+
+
+A = ["A", "B", "C", "D", "E"]
+B = ["C", "B", "E", "A", "D"]
+
+print(routeScore(A, B))
